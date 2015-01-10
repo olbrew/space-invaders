6893575b398745ffa5ee73b065494f35ff72414b
exatto <exatto0@gmail.com>
2015-01-10 14:50:53 +0100

    added bullet view

---
+++ src/view/Bullet.h
@@ -0,0 +1,24 @@
+#ifndef BULLET_SFML_H
+#define BULLET_SFML_H
+
+#include "../model/Bullet.h"
+#include <SFML/Graphics.hpp>
+#include <stdexcept>
+#include <iostream>
+#include <memory>
+
+namespace tysfml {
+    class Bullet: public ty::Bullet
+    {
+    public:
+        Bullet(const std::shared_ptr<Entity>);
+        const sf::Sprite& getSprite() const;
+    private:
+        void loadTexture();
+
+        sf::Texture _texture;
+        sf::Sprite _sprite;
+    };
+} /* namespace tysfml */
+
+#endif
