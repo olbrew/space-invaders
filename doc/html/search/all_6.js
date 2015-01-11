7d15fa0f21a7c41db4a1ac207d6110306361caf8
exatto <exatto0@gmail.com>
2015-01-12 00:03:20 +0100

    updated doc

--- doc/html/search/all_6.js
+++ doc/html/search/all_6.js
@@ -4,5 +4,6 @@ var searchData=
   ['main_2ecpp',['main.cpp',['../main_8cpp.html',1,'']]],
   ['menu',['Menu',['../classtysfml_1_1_menu.html',1,'tysfml']]],
   ['menu_2ecpp',['Menu.cpp',['../_menu_8cpp.html',1,'']]],
-  ['move',['move',['../classty_1_1_b_g_tile.html#ab84578439495e53b115f197915061620',1,'ty::BGTile::move()'],['../classty_1_1_bullet.html#a6c9eef353a7f7a206d810185cb7d7298',1,'ty::Bullet::move()'],['../classty_1_1_enemy_ship.html#a1076666593657fc02efd2f7cd295d087',1,'ty::EnemyShip::move()'],['../classty_1_1_player_ship.html#a690005677d92cd7065ceb2d980aa3aa5',1,'ty::PlayerShip::move()']]]
+  ['move',['move',['../classty_1_1_b_g_tile.html#ab84578439495e53b115f197915061620',1,'ty::BGTile::move()'],['../classty_1_1_bullet.html#a6c9eef353a7f7a206d810185cb7d7298',1,'ty::Bullet::move()'],['../classty_1_1_enemy_ship.html#a1076666593657fc02efd2f7cd295d087',1,'ty::EnemyShip::move()'],['../classty_1_1_player_ship.html#a690005677d92cd7065ceb2d980aa3aa5',1,'ty::PlayerShip::move()']]],
+  ['moveoffset',['moveOffset',['../classty_1_1_player_ship.html#ad99c26f03ceecc31741b57039b085955',1,'ty::PlayerShip']]]
 ];
--- doc/html/search/functions_7.js
+++ doc/html/search/functions_7.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['playdefaultlevel',['playDefaultLevel',['../classty_1_1_game.html#a8a7eeb272a18e84039751fc280a86160',1,'ty::Game']]],
   ['playership',['PlayerShip',['../classty_1_1_player_ship.html#a5cade88e288a3e4c449e57809cc8c217',1,'ty::PlayerShip::PlayerShip()'],['../classtysfml_1_1_player_ship.html#aac9e26bc519d05c4c14c3c80076fb579',1,'tysfml::PlayerShip::PlayerShip()']]],
   ['position',['Position',['../classutil_1_1_position.html#aa57723432f8b9fd8fc82f8b2e2198252',1,'util::Position::Position()'],['../classutil_1_1_position.html#a73b0c079fd2cea092a7565225ae68678',1,'util::Position::Position(const unsigned int, const unsigned int)'],['../classutil_1_1_position.html#ab30b1d1bbc0867c287b17dc822a622a5',1,'util::Position::Position(std::initializer_list&lt; unsigned int &gt;)']]]
 ];
