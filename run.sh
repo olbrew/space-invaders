09e23194999734983234146699e7f66b90cdeffd
olbrew <olbrew@users.noreply.github.com>
2016-01-02 18:42:33 +0100

    change cmake config and add run script to build and run release version

---
+++ run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# compile the game
+if  [ ! -d buildRelease ]
+then
+    mkdir buildRelease
+    cd buildRelease
+    cmake -DCMAKE_BUILD_TYPE=Release ..
+    make
+    make install
+fi
+
+# run the game
+./bin/space_invaders
